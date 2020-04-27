3bbde74eb86458cbdf0cced0024638372f980e5f
Pavitra-coder <pavitrabhalke456@gmail.com>
2020-04-27 23:12:06 +0530

    Sorting array in ascending order

---
+++ SortingInAscendingOrder.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+echo "Enter three numbers"                                 
+read  a
+read  b
+read  c
+declare -A arr
+arr[0]=`expr $(($a+$b*$c))`
+arr[1]=`expr $(($a*$b+$c))`
+arr[2]=`expr $(($c+$a/$b))`
+echo ${arr[@]}
+for (( i = 0; i<=3 ; i++ ))
+do
+   for (( j = $i; j<=3 ; j++ ))
+   do
+      if [ ${arr[$i]} -gt ${arr[$j]}  ]; then
+           t=${arr[$i]}
+           arr[$i]=${arr[$j]}
+           arr[$j]=$t
+      fi
+   done
+done
+
+#
+# Print the sorted number
+#
+echo -e "\nSorted Numbers in ascending Order:"
+for (( i=0; i<=3; i++ ))
+do
+  echo ${arr[$i]}
+done
